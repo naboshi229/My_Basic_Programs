6f38fdc01cc2d3a4767ac702d48dac7cecb643e8
naboshi <k.dybkowski@gmail.com>
2023-12-20 14:08:21 +0100

    Add files via upload

---
+++ Rock-Paper-Scissors-RPS/README.md
@@ -0,0 +1,10 @@
+# Rock-Paper-Scissors-RPS-
+Short an easy game of rock, paper and sicssors that I think everybody knows.
+This was my first time using modules. I divided my program into two files: main.py (which has all the lines of my code, I mean game) and ASCII_rps.py (which stores all of the ASCII character type of images, for rock, paper and sicissors).
+It contains random intiger (short for int) number generator (I imported here random module).
+Program also checks user input in two while loops.
+"Try" block in the first while loop checks if user typed intiger number (short for int) if not it asks them to type their input again expecting type of error "Value Error".
+User also can't type the number which is not 0, 1, 2, they will need to type their number again. Typing everything correctly will break all of the while loops.
+Program prints out the choice of user in ASCII character type of image imported from another python file, same goes for computer choice.
+Then program checks the result in (if statements) and prints out the result to user containing a proper message for every case.
+I think that will be it. Can't think of more what I can put into that program for now. Thanks for reading! :>
---
+++ Rock-Paper-Scissors-RPS/Rock-Paper-Scissors-RPS.py
@@ -0,0 +1,26 @@
+import random
+import ASCII_rps
+
+game_images = [ASCII_rps.rock, ASCII_rps.paper, ASCII_rps.scissors]
+
+while True:
+    try:
+        user_choice = int(input('What do you choose? Type "0" for Rock, "1" for Paper or "2" for Scissors.\n'))
+        while True:
+            if user_choice == 0 or user_choice == 1 or user_choice == 2:
+                break
+            else:
+                user_choice = int(input('A number is to high/low! Try only using [0,1,2]! :(\nType "0" for Rock, "1" for Paper or "2" for Scissors!\n'))
+        break
+    except ValueError:
+        print("Please enter a number, not a word or letter!")
+computer_choice = random.randint(0,2)
+print(game_images[user_choice])
+print(f"Computer chose:"+game_images[computer_choice])
+
+if user_choice == 0 and computer_choice == 2 or user_choice == 2 and computer_choice == 1 or user_choice == 1 and computer_choice == 0:
+    print("You win!!! Lessgo!!! :P")
+elif user_choice == 2 and computer_choice == 0 or user_choice == 1 and computer_choice == 2 or user_choice == 0 and computer_choice == 1:
+    print("You lose! :/\nGG and GL next time! :D")
+elif user_choice == computer_choice:
+    print("It's a draw!\nGL next time! :D")
