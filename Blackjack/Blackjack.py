93fea2dd6a49d2bc7cce0cc7687e09806d583bfd
naboshi <k.dybkowski@gmail.com>
2023-12-26 18:49:07 +0100

    Update Blackjack.py

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -1,6 +1,6 @@
 import os
 import random
-from Blackjakc_ART import logo
+from Blackjack_ART import logo
 def deal_card():
     """Returns a random card from the deck."""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
