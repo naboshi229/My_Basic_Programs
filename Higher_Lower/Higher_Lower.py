7b880be0181ae4958705f0f3b217819baf9f87af
naboshi <k.dybkowski@gmail.com>
2023-12-29 13:09:15 +0100

    Higher_Lower

---
+++ Higher_Lower/Higher_Lower.py
@@ -0,0 +1,39 @@
+import random, os
+from Higher_Lower_ART import logo, vs
+from Higher_Lower_DATA import data
+def format_data(account):
+    """Format the account data into printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+def check_answer(guess, a_followers, b_followers):
+    """Take the user guess and follower counts and returns if they got it right."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+print(logo)
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+while game_should_continue:
+    account_a = account_b
+    account_b = random.choice(data)
+    while account_a == account_b:
+        account_b = random.choice(data)
+    print(f"Compare A: {format_data(account_a)}.{vs}Against B: {format_data(account_b)}.")
+    guess = input('Who has more followers? Type "a" or "b": ').lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    clear()
+    print(logo)
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}.")
+    else:
+        game_should_continue = False
+        print(f"Sorry, that's wrong. Final score: {score}.")
---
+++ Higher_Lower/README.md
@@ -0,0 +1,8 @@
+# Higher_Lower
+## Description
+A small game in which user tries his best to guess who has more followers on instagram.
+## Quick note
+- Higher_Lower.py
+- Higher_Lower_ART.py
+- Higher_Lower_DATA.py
+### These are the files you need to download in order for my program to work properly.
