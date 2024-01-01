91b77aca6a22d6c60fcd0587a151942732ffe3db
naboshi <k.dybkowski@gmail.com>
2024-01-01 17:49:03 +0100

    Quiz_Game

---
+++ Quiz_Game/README.md
@@ -0,0 +1,9 @@
+# Quiz Game
+## Description
+Simple quiz game that was written in OOP (Object Oriented Programming) style.
+## Quick note
+- main.py
+- quiz_brain.py
+- question_model.py
+- data.py
+These are the files you need to download in order for my program to work properly. Feel free to edit data.py with different questions and answer to them.
---
+++ Quiz_Game/data.py
@@ -0,0 +1,42 @@
+question_data = [
+    {
+        "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The Windows ME operating system was released in the year 2000.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "The logo for Snapchat is a Bell.",
+        "correct_answer": "False",
+    },
+    {
+        "question": '"HTML"; stands for Hypertext Markup Language.',
+        "correct_answer": "True",
+    },
+    {
+        "question": "The Windows 7 operating system has six main editions.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "Linus Torvalds created Linux and Git.",
+        "correct_answer": "True",
+    },
+    {
+        "question": 'The Python programming language gets its name from the British comedy group "Monty Python"',
+        "correct_answer": "True",
+    },
+    {
+        "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+        "correct_answer": "False",
+    },
+    {
+        "question": "Ada Lovelace is often considered the first computer programmer.",
+        "correct_answer": "True",
+    },
+    {
+        "question": "RAM stands for Random Access Memory.",
+        "correct_answer": "True",
+    }
+]
