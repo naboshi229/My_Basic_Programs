7f1a79de42cfab02530943844d72d46a6f2729be
naboshi <k.dybkowski@gmail.com>
2024-01-18 00:02:05 +0100

    Update Bill-Calculator.py

--- Bill-Calculator/Bill-Calculator.py
+++ Bill-Calculator/Bill-Calculator.py
@@ -1,6 +1,6 @@
-bill = float(input("Welcome to the tip calculator.\nWhat was the total bill? $"))
-percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill? "))
+bill = float(input('Welcome to the tip calculator.\nWhat was the total bill? $'))
+percentage = int(input('What percentage tip would you like to give? 10, 12, or 15? '))
+people = int(input('How many people to split the bill? '))
 percentage /= 100
 tip = bill * (1 + percentage)
 result = tip / people
